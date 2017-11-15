1b898f1e78aee321f3d6f6efdc0b565b0699b684
Mathew Michaluk <mathewtmichaluk@gmail.com>
2017-11-15 15:14:56 +0000

    recovered missing admins

--- db/migrate/20171107214312_rename_column.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumn < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :products, :lastupdated, :last_updated
-    rename_column :products, :updatedby, :updated_by
-  end
-end
--- db/migrate/20171108012332_fix_columns.rb
+++
@@ -1,6 +0,0 @@
-class FixColumns < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :products, :last_updated
-    remove_column :products, :updated_by
-  end
-end
