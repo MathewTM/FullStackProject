6f6d66cfc89ccb963f16f9a9ccc145316df67561
Mathew Michaluk <mathewtmichaluk@gmail.com>
2017-11-27 02:12:59 +0000

    small fixes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    if !session[:cart] then session[:cart] = Hash.new
+    if !session[:cart] then session[:cart] = Hash.new end
 
     if params[:keyword] != nil then
       @products = Product.where("name LIKE :keyword OR description LIKE :keyword", {:keyword => "%" + params[:keyword] + "%"})
